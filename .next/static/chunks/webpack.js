7ef3613ccfb499be38cc7d316bd9076259b36143
Farley Piedrahita Orozco <farchodev@gmail.com>
2025-01-08 19:27:44 -0500

    Añadiendo modulo Sistemas operativos de red

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -161,7 +161,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c5d3d7e7806ae38b"; }
+/******/ 		__webpack_require__.h = function() { return "4dd68ff09cd3d840"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
